7bc5d028434d64f2d7337d375460c046fa7932fc
Sung Hoon Jung <44125480+sunghjung3@users.noreply.github.com>
2024-11-03 11:40:28 -0600

    Create vae.py

---
+++ vae.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+class VariationalEncoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(VariationalEncoder, self).__init__()
+        self.linear1 = nn.Linear(784, 512)
+        self.linear2 = nn.Linear(512, latent_dims)
+        self.linear3 = nn.Linear(512, latent_dims)
+
+        self.N = torch.distributions.Normal(0, 1)
+        self.N.loc = self.N.loc.cuda() # hack to get sampling on the GPU
+        self.N.scale = self.N.scale.cuda()
+        self.kl = 0
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        mu =  self.linear2(x)
+        sigma = torch.exp(self.linear3(x))
+        z = mu + sigma*self.N.sample(mu.shape)
+        self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+        return z
+
+def train(autoencoder, data, epochs=20):
+    opt = torch.optim.Adam(autoencoder.parameters())
+    for epoch in range(epochs):
+        for x, y in data:
+            x = x.to(device) # GPU
+            opt.zero_grad()
+            x_hat = autoencoder(x)
+            loss = ((x - x_hat)**2).sum() + autoencoder.encoder.kl
+            loss.backward()
+            opt.step()
+    return autoencoder
+
+
+def interpolate(autoencoder, x_1, x_2, n=12):
+    z_1 = autoencoder.encoder(x_1)
+    z_2 = autoencoder.encoder(x_2)
+    z = torch.stack([z_1 + (z_2 - z_1)*t for t in np.linspace(0, 1, n)])
+    interpolate_list = autoencoder.decoder(z)
+    interpolate_list = interpolate_list.to('cpu').detach().numpy()
+
+    w = 28
+    img = np.zeros((w, n*w))
+    for i, x_hat in enumerate(interpolate_list):
+        img[:, i*w:(i+1)*w] = x_hat.reshape(28, 28)
+    plt.imshow(img)
+    plt.xticks([])
+    plt.yticks([])
+
+
